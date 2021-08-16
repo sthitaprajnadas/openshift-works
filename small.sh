f3ae0478c86d7a584e93bb6301332023209170a1
spdas <sthitaprajnadas@gmail.com>
2021-08-17 00:53:09 +0530

    Update small.sh

--- small.sh
+++ small.sh
@@ -1,1 +1,7 @@
+#!/bin/sh
+for node in $(oc get nodes -o name);   # Iterating over each node in the cluster
+do
+	echo "====================== Node : ${node} ========"
+	oc adm node-logs ${node} --path=kube-apiserver/
 
+done
