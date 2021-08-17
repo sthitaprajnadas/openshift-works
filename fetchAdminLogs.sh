fdb06605f88d85923b1757b453d3a21321db1fa6
sthitaprajnadas <sthitaprajnadas@gmail.com>
2021-08-17 14:38:46 +0530

    "fix for unresponsive nodes"

--- fetchAdminLogs.sh
+++ fetchAdminLogs.sh
@@ -4,16 +4,23 @@
 #rm -rf adminlogs*.log  # Remove all old logs.Clears the working dir.
 for node in $(oc get nodes -o name);   # Iterating over each node in the cluster
 do
-    for logfile in $(oc adm node-logs ${node} --path=kube-apiserver | grep '.log')  # iterating over each log file in the node
-    do
-      OUTPUT="$(oc adm node-logs ${node} --path=kube-apiserver/${logfile} | jq  2>/dev/null)"  # added 2>/dev/null to ignore error
+    oc adm node-logs ${node} --path=kube-apiserver 2>/dev/null
+    if [ $? -eq 0 ]; then
 
-      if [ ! -z "${OUTPUT}" ]  # This to ignore the log files which dont have admin logs
-      then
-        echo "====================== Node : ${node} ======== File : ${logfile}=========================" 
-        oc adm node-logs ${node} --path=kube-apiserver/${logfile} | jq 'select(.user.username == "admin")'
-        #echo ${OUTPUT}    # yields same result as above but does not render the json properly
-        echo "===================End Node : ${node} ======== File : ${logfile}=========================" 
-      fi
-    done
+
+      for logfile in $(oc adm node-logs ${node} --path=kube-apiserver | grep '.log')  # iterating over each log file in the node
+      do
+        OUTPUT="$(oc adm node-logs ${node} --path=kube-apiserver/${logfile} | jq  2>/dev/null)"  # added 2>/dev/null to ignore error
+
+        if [ ! -z "${OUTPUT}" ]  # This to ignore the log files which dont have admin logs
+        then
+          echo "====================== Node : ${node} ======== File : ${logfile}=========================" 
+          oc adm node-logs ${node} --path=kube-apiserver/${logfile} | jq 'select(.user.username == "admin")'
+          #echo ${OUTPUT}    # yields same result as above but does not render the json properly
+          echo "===================End Node : ${node} ======== File : ${logfile}=========================" 
+        fi
+      done
+    else
+      echo "======================Node : ${node} is not accessible=========================" 
+    fi
 done
---
+++ fetchAdminLogs_bkp.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# usage - sh fetchAdminlog.sh 2>&1 | tee  adminlogs$(date "+%y%m%d_%H%M%S").log 
+
+#rm -rf adminlogs*.log  # Remove all old logs.Clears the working dir.
+for node in $(oc get nodes -o name);   # Iterating over each node in the cluster
+do
+    for logfile in $(oc adm node-logs ${node} --path=kube-apiserver | grep '.log')  # iterating over each log file in the node
+    do
+      OUTPUT="$(oc adm node-logs ${node} --path=kube-apiserver/${logfile} | jq  2>/dev/null)"  # added 2>/dev/null to ignore error
+
+      if [ ! -z "${OUTPUT}" ]  # This to ignore the log files which dont have admin logs
+      then
+        echo "====================== Node : ${node} ======== File : ${logfile}=========================" 
+        oc adm node-logs ${node} --path=kube-apiserver/${logfile} | jq 'select(.user.username == "admin")'
+        #echo ${OUTPUT}    # yields same result as above but does not render the json properly
+        echo "===================End Node : ${node} ======== File : ${logfile}=========================" 
+      fi
+    done
+done
