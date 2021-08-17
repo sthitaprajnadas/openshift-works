d5702f0e7face57f1ef64092aae1968214532445
spdas <sthitaprajnadas@gmail.com>
2021-08-17 22:05:56 +0530

    Update fetchAdminLogs.sh

--- fetchAdminLogs.sh
+++ fetchAdminLogs.sh
@@ -4,7 +4,7 @@
 #rm -rf adminlogs*.log  # Remove all old logs.Clears the working dir.
 for node in $(oc get nodes -o name);   # Iterating over each node in the cluster
 do
-    oc adm node-logs ${node} --path=kube-apiserver 2>/dev/null    #Aug 17: To first check if node is accessible.
+    oc adm node-logs ${node} --path=kube-apiserver >/dev/null 2>&1    #Aug 17: To first check if node is accessible.
     if [ $? -eq 0 ]; then  #Aug 17: Check if previous command gave no error (i.e. node is accessible).If it is then for those process logfiles
 
 
