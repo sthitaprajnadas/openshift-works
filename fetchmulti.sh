42394db53500bce4c7a216c72c0b384c38505b5c
spdas <sthitaprajnadas@gmail.com>
2021-08-17 23:55:41 +0530

    Create fetchmulti.sh

---
+++ fetchmulti.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# usage - sh fetchadminlog.sh 2>&1 > adminlogs$(date "+%y%m%d_%H%M%S").log 
+
+rm -rf adminlogs*.log  # Remove all old logs.Clears the working dir.
+nodes="crc-pkjt4-master-1 crc-pkjt4-master-2 crc-pkjt4-master-3 crc-pkjt4-master-0"
+for node in $nodes;   # Iterating over each node in the cluster
+do
+    oc adm node-logs ${node} --path=kube-apiserver >/dev/null 2>&1    #Aug 17: To first check if node is accessible.
+    if [ $? -eq 0 ]; then  #Aug 17: Check if previous command gave no error (i.e. node is accessible).If it is then for those process logfiles
+
+
+      for logfile in $(oc adm node-logs ${node} --path=kube-apiserver | grep '.log')  # iterating over each log file in the node
+      do
+        OUTPUT="$(oc adm node-logs ${node} --path=kube-apiserver/${logfile} | jq 'select(.user.username == "admin")'  2>/dev/null)"  # added 2>/dev/null to ignore error
+
+        if [ ! -z "${OUTPUT}" ]  # This to ignore the log files which dont have admin logs
+        then
+          echo "====================== Node : ${node} ======== File : ${logfile}=========================" 
+          oc adm node-logs ${node} --path=kube-apiserver/${logfile} | jq 'select(.user.username == "admin")'
+          #echo ${OUTPUT}    # yields same result as above but does not render the json properly
+          echo "===================End Node : ${node} ======== File : ${logfile}=========================" 
+        fi
+      done
+    else    # Aug 17: If node is inaccessible , then print it and move to next node
+      echo "======================Node : ${node} is not accessible=========================" 
+    fi
+done
