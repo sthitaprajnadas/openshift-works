fbcaffdc74b1b9c80120c89fd1bccfdc5513fc2c
tapasvit20 <tapasvit20@gmail.com>
2021-09-03 00:14:57 +0530

    fileintegrity modified

--- fileintegrity1.sh
+++ fileintegrity1.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
 ########################
-echo ""FileIntegrity CR status:"
-oc get fileintegrities $(oc get fileintegrities --namespace=openshift-file-integrity | grep -v NAME | awk '{ print $1}') -o jsonpath='{.status.phase}'
+echo "FileIntegrity CR status";
+#oc get fileintegrities $(oc get fileintegrities --namespace=openshift-file-integrity | grep -v NAME | awk '{ print $1}') -o jsonpath="{ .status.phase }'
+oc get fileintegrities/worker-fileintegrity  -o jsonpath="{ .status.phase }" --namespace=openshift-file-integrity
 
-    
+   
 sleep 1
 
-echo  "FileIntegrityNodeStatuses:"
+echo  "FileIntegrityNodeStatuses";
 
 #rm -rf fileintegritynodes
-pe  "oc get fileintegritynodestatuses.fileintegrity.openshift.io $(oc get fileintegritynodestatuses --namespace=openshift-file-integrity | grep -v NAME | awk '{ print $1}') -ojsonpath='{.items[*].results}' | jq -r"
+#pe  "oc get fileintegritynodestatuses.fileintegrity.openshift.io $(oc get fileintegritynodestatuses --namespace=openshift-file-integrity | grep -v NAME | awk '{ print $1}') -ojsonpath='{.items[*].results}' | jq -r"
 
-echo "FileIntegrityNodeStatus CR failed condition status:"
+echo "FileIntegrityNodeStatus CR failed condition status";
 
-pe "oc get fileintegritynodestatuses.fileintegrity.openshift.io  --namespace=openshift-file-integrity -ojsonpath='{.items[*].results}' | jq > nodes.json"
+oc get fileintegritynodestatuses.fileintegrity.openshift.io  --namespace=openshift-file-integrity -ojsonpath='{.items[*].results}' > nodes.json
 jq '.[].resultConfigMapName' nodes.json > failed
-sed -i 's/"//g'  failed
+sed -i 's/\"//g'  failed
 while IFS= read -r line; do
     if [ $line != 'null' ]
 	then
 		 oc describe cm $line
+	fi
 done < failed
 
-echo "get events:"
-pe "oc get events --field-selector reason=NodeIntegrityStatus --namespace=openshift-file-integrity"
+echo "check events";
+oc get events --field-selector reason=NodeIntegrityStatus --namespace=openshift-file-integrity
+echo "checked successfully";
